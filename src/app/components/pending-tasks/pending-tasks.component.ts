17568458204c27bfa88bbc1a34960f43b5d871aa
isharadilshan <dilshanishara75@gmail.com>
2019-08-30 10:58:04 +0530

    added priority logic for task cards

---
+++ src/app/components/pending-tasks/pending-tasks.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, query, stagger, animateChild } from '@angular/animations';
+
+
+@Component({
+  selector: 'app-pending-tasks',
+  templateUrl: './pending-tasks.component.html',
+  styleUrls: ['./pending-tasks.component.scss'],
+  animations: [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),  // initial
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
+          style({ transform: 'scale(1)', opacity: 1 }))  // final
+      ]),
+      transition(':leave', [
+        style({ transform: 'scale(1)', opacity: 1, height: '*' }),
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
+         style({ 
+           transform: 'scale(0.5)', opacity: 0, 
+           height: '0px', margin: '0px' 
+         })) 
+      ])
+    ]),
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(300, animateChild()))
+      ]),
+    ])
+  ]
+})
+export class PendingTasksComponent implements OnInit {
+
+  counter = 5;
+  list = [1,2,3,4,5];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  add(){
+    this.list.push(this.counter++);
+  }
+
+  remove(index) {
+    if(!this.list.length) return;
+    this.list.splice(index, 1);
+  }
+
+}
