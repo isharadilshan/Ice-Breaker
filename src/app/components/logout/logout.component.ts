897dbe5315ce2a6e811748106bb889ff8ed05148
isharadilshan <dilshanishara75@gmail.com>
2019-09-05 18:41:28 +0530

    firebase authentication added with email verification and password reset

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class LogoutComponent implements OnInit {
 
   constructor(public authService: AuthService) { }
 
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SigninComponent implements OnInit {
 
   constructor(public authService: AuthService) { }
 
