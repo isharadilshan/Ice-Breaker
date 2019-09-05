9bf9313d656847c86402655c6fd81cdf703d2d12
isharadilshan <dilshanishara75@gmail.com>
2019-09-05 10:57:40 +0530

    changed the UI of pending tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AdminpanelComponent } from './pages/adminpanel/adminpanel.component';
-import { LoginComponent } from './pages/login/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'admin-panel', component: AdminpanelComponent },
 ];
 
