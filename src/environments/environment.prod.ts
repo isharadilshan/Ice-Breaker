72b03ea3628b461d3f55262e02ff74bfbd990bc3
isharadilshan <dilshanishara75@gmail.com>
2019-09-06 10:39:55 +0530

    deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBH_W2aW5yy3b8JL6u4y1lQIM7_n3aLKtc",
+    authDomain: "ice-breaker-78d79.firebaseapp.com",
+    databaseURL: "https://ice-breaker-78d79.firebaseio.com",
+    projectId: "ice-breaker-78d79",
+    storageBucket: "",
+    messagingSenderId: "64330276810",
+    appId: "1:64330276810:web:b35ef477c9f8deb8"
+  }
 };
