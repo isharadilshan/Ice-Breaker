2a8a885f573c3ab0f8459991649999323c160b56
isharadilshan <dilshanishara75@gmail.com>
2019-09-08 13:52:45 +0530

    implemented UIs for authentication

--- src/app/pages/adminpanel/adminpanel.component.ts
+++ src/app/pages/adminpanel/adminpanel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-adminpanel',
@@ -8,7 +7,7 @@ import { AuthService } from 'src/app/shared/auth.service';
 })
 export class AdminpanelComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
   }
