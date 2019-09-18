e144765f8efeeb474b2875365651fd088b755d01
isharadilshan <dilshanishara75@gmail.com>
2019-09-18 16:52:24 +0530

    changed the animation of pending tasks

--- src/app/components/devices/device-list/device-list.component.ts
+++ src/app/components/devices/device-list/device-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DeviceService } from 'src/app/shared/utils/device.service';
+import { MatDialog, MatDialogConfig, MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { NotificationService } from 'src/app/shared/notification/notification.service';
+import { DeviceComponent } from '../device/device.component';
 
 @Component({
   selector: 'app-device-list',
@@ -7,9 +12,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, private service: DeviceService, private dialog: MatDialog, private notificationService: NotificationService) { }
 
+  listData: MatTableDataSource<Project>;
+  displayedColumns: string[] = ['title','code','actions'];
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  searchKey: string;
+  
   ngOnInit() {
+
+    this.service.getDevices().subscribe(
+      list => {
+        let array = list.map(item => {
+          return {
+            $key: item.key,
+            ...item.payload.val()
+          };
+        });
+
+        this.listData = new MatTableDataSource(array);
+        this.listData.sort = this.sort;
+        this.listData.paginator = this.paginator;
+        this.listData.filterPredicate = (data,filter) => {
+          return this.displayedColumns.some(ele => {
+            return ele != 'actions' && data[ele].toLowerCase().indexOf(filter) != -1;
+          })  
+        };
+
+      });
   }
+
+  onSearchClear(){
+    this.searchKey="";
+    this.applyFilter();
+  }
+
+  applyFilter(){
+    this.listData.filter = this.searchKey.trim().toLowerCase();
+  }
+
+  onCreate(){
+    this.service.initializeFormGroup();
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "60%";
+    this.dialog.open(DeviceComponent,dialogConfig);
+  }
+
+  onEdit(row){
+    this.service.populateForm(row);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "60%";
+    this.dialog.open(DeviceComponent,dialogConfig);
+  }
+
+  onDelete($key){
+    if(confirm('Are you sure to delete this record ?')){
+      this.service.deleteDevice($key);
+      this.notificationService.warn('Deleted Successfully');
+    }
+  }
+
+  //SOLID Principle 
+  signOut(){
+    try{
+      this.authService.signOut();
+    }catch(err){
+      console.log("Error occurs when trying to sign out ///"+err);
+    }
+  }
+
+}
+
+export class Project{
+  title: string;
+  code: string;
 
 }
